97fd624e63fa1a5b3881e55dcfd004c07dde5793
udityaprakash <udityaprakash983@gmail.com>
2024-02-15 19:13:02 +0530

    logs implemented

--- index.js
+++ index.js
@@ -17,12 +17,14 @@ const bot = new Telegraf(BOT_TOKEN);
 
 bot.start((ctx) =>{
     const username = ctx.message.from.username;
+    console.log("new user joined to bot named "+username);
     ctx.reply('Welcome '+ username +'! Send Sylva text and get a voice note in return.');
 });
 
 bot.on('text', async ctx => {
     try {
         const text = ctx.message.text;
+        console.log("Message from " + ctx.message.from.first_name + text);
         const voiceNote = await convertTextToVoice(text);
         const audio = fs.readFileSync(voiceNote + ".mp3");
         ctx.replyWithVoice({ source: audio });
